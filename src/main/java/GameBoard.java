346c2669cb3f35f3f344cbdfdabb69825b19b0c7
Joseph Arrieta <jac.wshmstr@gmail.com>
2013-12-15 21:05:05 -0430

    Change GameBoard.java
    -add tracking the winning combination.

--- src/main/java/GameBoard.java
+++ src/main/java/GameBoard.java
@@ -8,54 +8,114 @@ package main.java;
  * To change this template use File | Settings | File Templates.
  */
 public class GameBoard {
-    private String [][] board;
+    private String[][] board;
+    private String[] winnerCombination;
+
 
     public GameBoard() {
-          board = new String[3][3];
+        winnerCombination = new String[3];
+        initializeGameBoard();
+    }
+
+    private void initializeGameBoard() {
+        board = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = "";
+            }
+        }
+    }
+
+    public String[] getWinnerCombination() {
+        return winnerCombination;
     }
 
     public void markBoard(Player player, Point point) {
-              board[point.getRow()][point.getColumn()] = player.getMark();
+        board[point.getRow()][point.getColumn()] = player.getMark();
     }
 
-    public String getMarkAtPoint(Point point){
-           return board[point.getRow()][point.getColumn()];
+    public String getMarkAtPoint(Point point) {
+        return board[point.getRow()][point.getColumn()];
     }
 
-    public boolean isWinner(Player player){
+    public boolean isWinner(Player player) {
         return checkDiagonals(player) || checkColumns(player) || checkRows(player);
     }
 
     private boolean checkDiagonals(Player player) {
-        return (board[0][0].equals(player.getMark()) &&
+        boolean isWinner = false;
+        if ((board[0][0].equals(player.getMark()) &&
                 board[1][1].equals(player.getMark()) &&
-                board[2][2].equals(player.getMark())) ||
-               (board[0][2].equals(player.getMark()) &&
+                board[2][2].equals(player.getMark()))) {
+            winnerCombination[0] = "0 0";
+            winnerCombination[1] = "1 1";
+            winnerCombination[2] = "2 2";
+            isWinner = true;
+
+        } else if ((board[0][2].equals(player.getMark()) &&
                 board[1][1].equals(player.getMark()) &&
-                board[2][0].equals(player.getMark()));
+                board[2][0].equals(player.getMark()))) {
+            winnerCombination[0] = "0 2";
+            winnerCombination[1] = "1 1";
+            winnerCombination[2] = "2 0";
+            isWinner = true;
+        }
+        return isWinner;
     }
 
     private boolean checkColumns(Player player) {
-        return (board[0][0].equals(player.getMark()) &&
+        boolean isWinner = false;
+        if ((board[0][0].equals(player.getMark()) &&
                 board[0][1].equals(player.getMark()) &&
-                board[0][2].equals(player.getMark())) ||
-               (board[1][0].equals(player.getMark()) &&
+                board[0][2].equals(player.getMark()))) {
+            winnerCombination[0] = "0 0";
+            winnerCombination[1] = "0 1";
+            winnerCombination[2] = "0 2";
+            isWinner = true;
+        } else if ((board[1][0].equals(player.getMark()) &&
                 board[1][1].equals(player.getMark()) &&
-                board[1][2].equals(player.getMark())) ||
-               (board[2][0].equals(player.getMark()) &&
+                board[1][2].equals(player.getMark()))) {
+            winnerCombination[0] = "1 0";
+            winnerCombination[1] = "1 1";
+            winnerCombination[2] = "1 2";
+            isWinner = true;
+        } else if ((board[2][0].equals(player.getMark()) &&
                 board[2][1].equals(player.getMark()) &&
-                board[2][2].equals(player.getMark()));
+                board[2][2].equals(player.getMark()))) {
+            winnerCombination[0] = "2 0";
+            winnerCombination[1] = "2 1";
+            winnerCombination[2] = "2 2";
+            isWinner = true;
+        }
+        return isWinner;
     }
 
-    private boolean checkRows(Player player){
-        return (board[0][0].equals(player.getMark()) &&
+    private boolean checkRows(Player player) {
+        boolean isWinner = false;
+        if ((board[0][0].equals(player.getMark()) &&
                 board[1][0].equals(player.getMark()) &&
-                board[2][0].equals(player.getMark())) ||
-               (board[0][1].equals(player.getMark()) &&
+                board[2][0].equals(player.getMark()))) {
+            winnerCombination[0] = "0 0";
+            winnerCombination[1] = "1 0";
+            winnerCombination[2] = "2 0";
+            isWinner = true;
+        } else if ((board[0][1].equals(player.getMark()) &&
                 board[1][1].equals(player.getMark()) &&
-                board[2][1].equals(player.getMark())) ||
-               (board[0][2].equals(player.getMark()) &&
+                board[2][1].equals(player.getMark()))) {
+            winnerCombination[0] = "0 1";
+            winnerCombination[1] = "1 1";
+            winnerCombination[2] = "2 1";
+            isWinner = true;
+        } else if ((board[0][2].equals(player.getMark()) &&
                 board[1][2].equals(player.getMark()) &&
-                board[2][2].equals(player.getMark()));
+                board[2][2].equals(player.getMark()))) {
+            winnerCombination[0] = "0 2";
+            winnerCombination[1] = "1 2";
+            winnerCombination[2] = "2 2";
+            isWinner = true;
+        }
+        return isWinner;
     }
+
+
 }
---
+++ src/main/java/TicTacToeUI.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jac
+ * Date: 12/14/13
+ * Time: 11:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TicTacToeUI {
+}
